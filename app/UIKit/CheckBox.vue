2335203b35badd2ca2b72310c3be9fc9e8085665
ikostenevich <ikostenevich.dev@gmail.com>
2025-08-02 18:16:11 +0300

    fix: checkbox

--- app/UIKit/CheckBox.vue
+++ app/UIKit/CheckBox.vue
@@ -1,19 +1,13 @@
-<script setup lang="ts">
-import Icon from './Icon.vue';
-
-defineProps<{
-  id: string;
-  modelValue: boolean;
-}>();
-
-const emit = defineEmits(['update:modelValue']);
-</script>
-
 <template>
-  <label :for="id" class="relative flex cursor-pointer items-center">
+  <label
+    :for="id"
+    class="relative flex w-max items-center"
+    :class="disabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer'"
+  >
     <input
       :id="id"
       type="checkbox"
+      :disabled="disabled"
       :checked="modelValue"
       class="peer sr-only"
       @change="
@@ -21,16 +15,32 @@ const emit = defineEmits(['update:modelValue']);
       "
     />
     <div
-      class="flex h-6 w-6 items-center justify-center rounded border-2 border-dark-default bg-white transition-colors"
+      class="flex h-6 w-6 items-center justify-center rounded border-2 border-dark bg-white transition-colors"
+      role="checkbox"
+      :aria-checked="modelValue"
+      aria-hidden="false"
     >
       <Icon
         name="checked"
         class="text-primary transition-transform duration-200"
+        :size="16"
         :class="modelValue ? 'scale-100 opacity-100' : 'scale-0 opacity-0'"
       />
     </div>
-    <div class="ml-2">
+    <div v-if="$slots.default" class="ml-4 text-gray">
       <slot />
     </div>
   </label>
 </template>
+
+<script setup lang="ts">
+import Icon from './Icon.vue';
+
+defineProps<{
+  id: string;
+  modelValue: boolean;
+  disabled?: boolean;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+</script>
