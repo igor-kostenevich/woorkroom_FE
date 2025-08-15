50085b292512aba987c0961efa460c973425442e
Yehor Hert <egorkagert@gmail.com>
2025-08-15 12:13:54 +0200

    fix: add import  defineAsyncComponent

--- app/components/common/UserAvatar.vue
+++ app/components/common/UserAvatar.vue
@@ -35,7 +35,7 @@
 </template>
 
 <script setup lang="ts">
-import Progress from '~/UIkit/Progress.vue';
+const Progress = defineAsyncComponent(() => import('~/UIKit/Progress.vue'));
 
 const props = defineProps<{
   progress?: number;
