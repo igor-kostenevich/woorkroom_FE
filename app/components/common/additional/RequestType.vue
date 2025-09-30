43a254679d25ebbc006d997d8fb29ce298ef7152
Yehor Hert <egorkagert@gmail.com>
2025-09-30 21:53:40 +0200

    fix: resolve problem with EntityCardTable and create new component

--- app/components/common/additional/RequestType.vue
+++ app/components/common/additional/RequestType.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
-    <div class="mb-1 text-sm text-gray-light">
+    <div v-if="showLabel" class="mb-1 text-sm text-gray-light">
       {{ $t('additional.requestType') }}
     </div>
-    <div class="">
-      <VacationIndicator status="pending" indicator="sick" />
+    <div class="flex items-center gap-3">
+      <div :class="['h-[10px] w-[10px] rounded-full', colorType]" />
+      <div class="font-bold leading-[150%] text-dark">
+        {{ $t(`indicator.${requestType}`) }}
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const VacationIndicator = defineAsyncComponent(
-  () => import('~/components/pages/vacations/VacationIndicator.vue')
+import type { TLeaveType } from '~/types/vacations/VacationIndicator';
+
+const props = withDefaults(
+  defineProps<{
+    requestType: TLeaveType;
+    showLabel?: boolean;
+  }>(),
+  {
+    showLabel: true,
+  }
 );
 
-// interface  {
-//
-// }
-//
-// withDefaults(defineProps<>(), {
-//
-// });
+const colorType = computed(() => {
+  return {
+    'bg-red': props.requestType === 'sick',
+    'bg-violet': props.requestType === 'remote',
+    'bg-blue': props.requestType === 'vacation',
+  };
+});
 </script>
