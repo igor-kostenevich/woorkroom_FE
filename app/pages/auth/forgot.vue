a45eff96614b6909d19bf3569fe4b7b301f8f984
Yehor Hert <egorkagert@gmail.com>
2025-10-26 07:07:26 +0100

    create forgot page

--- app/pages/auth/forgot.vue
+++ app/pages/auth/forgot.vue
@@ -2,7 +2,6 @@
 import Icon from '~/UIKit/Icon.vue';
 
 const Input = defineAsyncComponent(() => import('@/UIKit/Input.vue'));
-const Checkbox = defineAsyncComponent(() => import('@/UIKit/CheckBox.vue'));
 const Button = defineAsyncComponent(() => import('@/UIKit/Button.vue'));
 const LinkButton = defineAsyncComponent(() => import('@/UIKit/LinkButton.vue'));
 
@@ -10,11 +9,8 @@ definePageMeta({
   layout: 'auth',
 });
 
-const rememberUser = ref(false);
-
 const form = ref({
   email: '',
-  password: '',
 });
 const rules = {
   email: {
@@ -23,16 +19,6 @@ const rules = {
       validators.required
     ),
     email: helpers.withMessage($t('validation.invalidEmail'), validators.email),
-  },
-  password: {
-    required: helpers.withMessage(
-      $t('validation.required'),
-      validators.required
-    ),
-    minLength: helpers.withMessage(
-      $t('validation.minLength'),
-      validators.minLength(8)
-    ),
   },
 };
 
@@ -83,38 +69,14 @@ const loginValidation = async () => {
           {{ $t('auth.signInWoorkroom') }}
         </h2>
         <div class="mb-7 md:mb-9 lg:mb-7 xl:mb-9">
-          <div class="mb-6 md:mb-7 lg:mb-5 xl:mb-7">
-            <Input v-model="form.email" placeholder="youremail@gmail.com">
-              <template #topTextLeft>{{ $t('email') }}</template>
-              <template v-if="validationErrors.email.message" #errorMessage>
-                {{ validationErrors.email.message }}
-              </template>
-            </Input>
-          </div>
-          <div>
-            <Input
-              v-model="form.password"
-              type="password"
-              placeholder="Password"
-            >
-              <template #topTextLeft>{{ $t('password') }}</template>
-              <template v-if="validationErrors.password.message" #errorMessage>
-                {{ validationErrors.password.message }}
-              </template>
-            </Input>
-          </div>
+          <Input v-model="form.email" placeholder="youremail@gmail.com">
+            <template #topTextLeft>{{ $t('email') }}</template>
+            <template v-if="validationErrors.email.message" #errorMessage>
+              {{ validationErrors.email.message }}
+            </template>
+          </Input>
         </div>
-        <div class="mb-11 flex justify-between gap-7 md:mb-12 lg:mb-9 xl:mb-12">
-          <div class="flex items-center gap-4">
-            <Checkbox v-model="rememberUser" />
-            <div class="text-gray">{{ $t('rememberMe') }}</div>
-          </div>
-          <div>
-            <LinkButton to="/auth/forgot" color="secondary">{{
-              $t('forgotPassword')
-            }}</LinkButton>
-          </div>
-        </div>
+
         <div class="flex flex-col items-center">
           <Button
             color="primary"
@@ -122,9 +84,9 @@ const loginValidation = async () => {
             class="mb-5 w-full min-w-[170px] md:w-auto"
             @click="loginValidation"
           >
-            {{ $t('auth.signIn') }}
+            {{ $t('restorePassword') }}
           </Button>
-          <LinkButton>{{ $t('auth.dontHaveAccount') }}</LinkButton>
+          <LinkButton to="/auth/login">{{ $t('haveAccount') }}</LinkButton>
         </div>
       </div>
     </div>
