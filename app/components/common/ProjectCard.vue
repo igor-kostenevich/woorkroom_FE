ff101a5e2cfdf99580e3aa58292fd4a921fccfd4
ikostenevich <ikostenevich.dev@gmail.com>
2026-03-01 18:36:49 +0200

    hotfix

--- app/components/common/ProjectCard.vue
+++ app/components/common/ProjectCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { IProjectCard } from '~/types/profile/projectCard';
+import type { IProjectCard } from '~/types/profile/projectCardtest';
 
 const PriorityBadge = defineAsyncComponent(
   () => import('~/components/common/additional/Priority.vue')
